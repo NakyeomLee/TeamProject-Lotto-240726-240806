41886347eea73f205dbf7d0fca1846803ba03b16
MooHyunPark <angus1208@naver.com>
2024-07-26 18:42:47 +0900

    자동,반자동,수동버튼 기능을 구현해주는 메소드 추가
    2번째 커밋.

--- team project2 Lotto/src/FunctionList.java
+++ team project2 Lotto/src/FunctionList.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -22,7 +24,24 @@ public class FunctionList extends JFrame {
 //			lblList.add(lbl);
 //			pnl.add(lbl);
 //		}
-//
+//		JButton btn1 = new JButton("자동");
+//		JButton btn2 = new JButton("수동");
+//		JButton btn3 = new JButton("반자동");
+//		pnl.add(btn1);
+//		pnl.add(btn2);
+//		pnl.add(btn3);
+//		List<JCheckBox> checkBoxList = new ArrayList<>();
+//		for (int i = 0; i < 45; i++) {
+//			JCheckBox checkBox = new JCheckBox(String.valueOf(i));
+//			checkBox.setEnabled(false);
+//			checkBoxList.add(checkBox);
+//			pnl.add(checkBox);
+//		}
+//		
+//		autoOrSemiAutoBtnFuntion(btn1, checkBoxList, "auto");
+//		autoOrSemiAutoBtnFuntion(btn2, checkBoxList, "self");
+//		autoOrSemiAutoBtnFuntion(btn3, checkBoxList, "semiAuto");
+//		
 //		changeToLabelVisible(lblList.get(0), lblList.get(1), lblList.get(2), lblList.get(3),
 //				lblList.get(4), lblList.get(5), lblList.get(6));
 //
@@ -69,30 +88,97 @@ public class FunctionList extends JFrame {
 						break;
 					}
 					boolean allVisible = true;
-	                for (JLabel lbl1 : list) {
-	                    if (!lbl1.isVisible()) {
-	                        allVisible = false;
-	                        break;
-	                    }
-	                }
-
-	                if (allVisible) {
-	                    ((Timer)arg0.getSource()).stop();
-	                }
+					for (JLabel lbl1 : list) {
+						if (!lbl1.isVisible()) {
+							allVisible = false;
+							break;
+						}
+					}
+					if (allVisible) {
+						((Timer) arg0.getSource()).stop();
+					}
 				}
 			}
 		});
 		timer.start();
 	}
+
+	// 자동 , 반자동, 수동 버튼을 눌렀을 때의 구현 기능 메소드 입니다.
+	// 괄호 안에는 자동 버튼, List, 옵션을 넣습니다. (List는 체크박스들을 가지고 있어야 합니다.)
+	// 옵션은 String(문자열)로 작성하시면 되고, auto를 입력하시면 자동버튼의 기능,
+	// semiAuto를 입력하시면 반자동버튼의 기능을 가집니다.
+	// self를 입력하면 수동버튼 기능을 가집니다.
+	public void autoOrSemiAutoBtnFuntion(JButton btn, List<JCheckBox> checkBoxList, String option) {
+
+		if (option.equals("auto")) {
+			
+			btn.addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+					changecheckBoxEnableToTrue(checkBoxList);
+					for (int i = 0; i < checkBoxList.size(); i++) {
+						checkBoxList.get(i).setSelected(false);
+					}
+					selectCheckBox(checkBoxList, 0);
+				}
+			});
+		} else if (option.equals("semiAuto")) {
+			btn.addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+					changecheckBoxEnableToTrue(checkBoxList);
+					int count = 0;
+					for (int i = 0; i < checkBoxList.size(); i++) {
+						if (checkBoxList.get(i).isSelected()) {
+							count++;
+						}
+					}
+					selectCheckBox(checkBoxList, count);
+				}
+			});
+		} else if(option.equals("self")) {
+			btn.addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					changecheckBoxEnableToTrue(checkBoxList);
+					
+					for (int i = 0; i < checkBoxList.size(); i++) {
+						checkBoxList.get(i).setSelected(false);
+					}
+				}
+			});
+		}
+	}
+
 	
-	
+
+	// 체크박스를 선택하는 기능 중 중복된 내용을 메소드화
+	private void selectCheckBox(List<JCheckBox> checkBoxList, int count) {
+		while (count < 6) {
+			Random random = new Random();
+			int index = random.nextInt(45);
+			if (!checkBoxList.get(index).isSelected()) {
+				checkBoxList.get(index).setSelected(true);
+				count++;
+			}
+		}
+	}
+	// 체크박스를 선택하는 기능 중 중복된 내용을 메소드화
+	private void changecheckBoxEnableToTrue(List<JCheckBox> checkBoxList) {
+		for (int i = 0; i < checkBoxList.size(); i++) {
+			checkBoxList.get(i).setEnabled(true);
+		}
+	}
 
 //	public static void main(String[] args) {
 //		new FunctionList().setVisible(true);
-
+//
 //		
 //		List<Integer> list = f.resultLottoNumber();
 //		System.out.println(list);
-
+//
 //	}
 }
