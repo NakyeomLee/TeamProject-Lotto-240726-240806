ac64b328e047b988c272aa3f61b144558ad18a6c
qwer123toy <qwer123toy@gmail.com>
2024-07-29 14:41:36 +0900

    메인부분 수정
    GIF 수정 및 이미지 추가

--- team project2 Lotto/src/MainPnl.java
+++ team project2 Lotto/src/MainPnl.java
@@ -1,23 +1,33 @@
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
+
+import java.util.TimerTask;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.MediaTracker;
+
 import javax.swing.ImageIcon;
 import javax.swing.SwingConstants;
+import javax.swing.Timer;
 
 // 메인 창
 // 작업자 : 이재민
 // 기본 구성요소 : 콤보박스, 텍스트, DialogPnl을 호출하는 버튼
 public class MainPnl extends JFrame {
 	private int lottoPlayCount = 1;
-	
-	
+
 	public MainPnl() {
 		super("인생역전 로또");
 		setSize(500, 500);
@@ -28,28 +38,45 @@ public class MainPnl extends JFrame {
 		JPanel centerPanel = new JPanel();
 		getContentPane().add(centerPanel);
 
-		
-//		JLabel lblNewLabel_1 = new JLabel("New label");
-//		centerPanel.add(lblNewLabel_1);
-		
 		centerPanel.setLayout(new BorderLayout(0, 0));
-		
+
 		JPanel panel = new JPanel();
 		centerPanel.add(panel, BorderLayout.CENTER);
 		panel.setLayout(new BorderLayout(0, 0));
-		
-		JLabel lblPicture = new JLabel("");
-		lblPicture.setIcon(new ImageIcon(MainPnl.class.getResource("/image/background.jpg")));
-		lblPicture.setHorizontalAlignment(SwingConstants.CENTER);
-		panel.add(lblPicture, BorderLayout.CENTER);
-		
+
+		JLayeredPane layeredPane = new JLayeredPane();
+
+		JLabel gifLabel = new JLabel("");
+
+		ImageIcon gifIcon = new ImageIcon(MainPnl.class.getResource("/image/background5.gif"));
+		gifLabel.setIcon(gifIcon);
+		gifLabel.setBounds(0, 0, gifIcon.getIconWidth(), gifIcon.getIconHeight());
+		layeredPane.add(gifLabel, Integer.valueOf(1));
+
+		Timer timer = new Timer(50, new ActionListener() {
+			int count = 0;
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				count++;
+				if (count == 18) {
+					ImageIcon jpgIcon = new ImageIcon(MainPnl.class.getResource("/image/background.jpg"));
+					gifLabel.setIcon(jpgIcon);
+					((Timer) e.getSource()).stop();
+				}
+			}
+		});
+
+		timer.start();
+
+		centerPanel.add(layeredPane);
+
 		JPanel panel_1 = new JPanel();
 		centerPanel.add(panel_1, BorderLayout.NORTH);
-		
-		JLabel lblLottoCount = new JLabel("로또 수량을 선택하세요. ");
+
+		JLabel lblLottoCount = new JLabel("알아서 사세요");
 		panel_1.add(lblLottoCount);
-		
-		
+
 //		String[] items = { "로또 수량을 선택하세요.", "1", "2", "3", "4", "5" };
 //		JComboBox combo = new JComboBox();
 //		panel_1.add(combo);
@@ -76,6 +103,9 @@ public class MainPnl extends JFrame {
 					DialogPnl dialogPnl = new DialogPnl(lottoCountInteger, MainPnl.this);
 					dialogPnl.setVisible(true);
 				}
+				else {
+					JOptionPane.showMessageDialog(MainPnl.this, "숫자를 선택해주세요.");
+				}
 			}
 		});
 
@@ -86,13 +116,15 @@ public class MainPnl extends JFrame {
 		JLabel lblNewLabel = new JLabel("제 " + lottoPlayCount + "회 인생 역전 로또");
 		lblNewLabel.setHorizontalAlignment(JLabel.CENTER);
 
+		revalidate();
+		repaint();
 
 		northPanel.add(lblNewLabel);
-		
+
 		JPanel panel_2 = new JPanel();
 		northPanel.add(panel_2, BorderLayout.NORTH);
 		panel_2.setLayout(new BorderLayout(0, 0));
-		
+
 		JLabel lblNewLabel_1 = new JLabel("인생역전 로또!");
 		lblNewLabel_1.setHorizontalAlignment(JLabel.RIGHT);
 		panel_2.add(lblNewLabel_1);
