ee0f064d460441e9cff846e7e3537354f5b38165
NakyeomLee <dlsrud0620@naver.com>
2024-07-29 12:11:24 +0900

    다이얼로그 창 7차

--- team project2 Lotto/src/DialogPnl.java
+++ team project2 Lotto/src/DialogPnl.java
@@ -150,7 +150,7 @@ public class DialogPnl extends JDialog {
 		});
 
 		pack();
-		setSize(500, 500);
+        setSize(500, 500);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
 //	public static void main(String[] args) {
