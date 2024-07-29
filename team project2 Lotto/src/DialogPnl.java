ac1530059648c31dcab70c5d63d05cbd159d576c
NakyeomLee <dlsrud0620@naver.com>
2024-07-29 14:23:46 +0900

    다이얼로그 창 9차

--- team project2 Lotto/src/DialogPnl.java
+++ team project2 Lotto/src/DialogPnl.java
@@ -2,6 +2,7 @@ import java.awt.BorderLayout;
 import java.awt.CardLayout;
 
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -13,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
+import java.awt.GridLayout;
 
 // 작업자 : 이나겸
 // 기본 구성요소 : 로또번호를 정하는 창, 로또의 결과값이 나오는 창, 결과를 출력하는 창
@@ -27,30 +29,53 @@ public class DialogPnl extends JDialog {
 		setModal(true); // 다이얼로그 창 닫기 전까지 다른 동작 불가
 
 		CardLayout cardLayout = new CardLayout();
-		BorderLayout borderLayout = new BorderLayout();
+//		BorderLayout borderLayout = new BorderLayout();
 
 		JPanel centerPanel = new JPanel(); // 모든 패널이 다 포함되는 패널(밑바탕)
 		centerPanel.setLayout(cardLayout);
+
 		JPanel buyLottoPanel = new JPanel(); // 로또 구매 창 패널
-		buyLottoPanel.setLayout(borderLayout);
+//		buyLottoPanel.setLayout(borderLayout);
+
 		JPanel numberCheckPanel = new JPanel(); // 당첨 숫자 확인 창 패널
-		numberCheckPanel.setLayout(null);
+//		numberCheckPanel.setLayout(null);
+
 		JPanel resultCheckPanel = new JPanel(); // 결과 확인 창 패널
-		resultCheckPanel.setLayout(null);
+//		resultCheckPanel.setLayout(null);
 
 		// 로또 구매 창 패널에 들어갈 요소들
 		JLabel textLabel = new JLabel("인생역전 로또"); // 인생 역전 로또 레이블
 
-		JPanel includeNumChoicePanel = new JPanel(); // 번호 선택 패널과 자동 수동 반자동 버튼들 포함될 패널
-		includeNumChoicePanel.setLayout(null);
+		for (int i = 0; i < Integer.valueOf(lottoCount); i++) {
+			JPanel includeNumChoicePanel = new JPanel(); // 번호 선택 패널, 자동 수동 반자동 버튼들이 들어있는 패널이 포함될 패널
+//			includeNumChoicePanel.setLayout(null);
 
-		JPanel numChoicePanel = new JPanel(); // 번호 선택 패널
+			JPanel includeButtonsPanel = new JPanel(); // 자동 수동 반자동 버튼들 포함될 패널
+
+			JPanel numChoicePanel = new JPanel(); // 번호 선택 패널
+			numChoicePanel.setLayout(new GridLayout(0, 5, 5, 5)); // GridLayout
+
+			// 사용자가 선택할 번호 체크박스 (1 ~ 45)
+			List<JCheckBox> checkNumList = new ArrayList<>();
+			for (int j = 1; j <= 45; j++) {
+				checkNumList.add(new JCheckBox(String.valueOf(j)));
+			}
+
+			JButton outoButton = new JButton("자동"); // 자동 버튼
+			JButton selfButton = new JButton("수동"); // 수동 버튼
+			JButton halfOutoButton = new JButton("반자동"); // 반자동 버튼
+
+			// 로또 구매 창 패널에 요소들 add
+			buyLottoPanel.add(includeNumChoicePanel);
+			includeNumChoicePanel.add(includeButtonsPanel);
+			includeNumChoicePanel.add(numChoicePanel);
+			includeButtonsPanel.add(outoButton);
+			includeButtonsPanel.add(selfButton);
+			includeButtonsPanel.add(halfOutoButton);
+		}
 
 		JPanel includeSendButtonPanel = new JPanel(); // 번호 제출 버튼이 포함될 패널
 
-		JButton outoButton = new JButton("자동"); // 자동 버튼
-		JButton selfButton = new JButton("수동"); // 수동 버튼
-		JButton halfOutoButton = new JButton("반자동"); // 반자동 버튼
 		JButton sendButton = new JButton("번호 제출"); // 번호 제출 버튼
 
 		// 당첨 숫자 확인 창 패널에 들어갈 요소들
@@ -82,13 +107,8 @@ public class DialogPnl extends JDialog {
 		JButton closeButton = new JButton("종료"); // 종료 버튼
 
 		// 로또 구매 창 패널에 요소들 add
-		buyLottoPanel.add(textLabel, borderLayout.NORTH);
-		buyLottoPanel.add(includeNumChoicePanel, borderLayout.CENTER);
-		buyLottoPanel.add(includeSendButtonPanel, borderLayout.SOUTH);
-		includeNumChoicePanel.add(numChoicePanel);
-		includeNumChoicePanel.add(outoButton);
-		includeNumChoicePanel.add(selfButton);
-		includeNumChoicePanel.add(halfOutoButton);
+		buyLottoPanel.add(textLabel);
+		buyLottoPanel.add(includeSendButtonPanel);
 		includeSendButtonPanel.add(sendButton);
 
 		// 당첨 숫자 확인 창 패널에 요소들 add
@@ -126,9 +146,7 @@ public class DialogPnl extends JDialog {
 			public void actionPerformed(ActionEvent e) {
 				cardLayout.show(centerPanel, "NumberCheck");
 
-				functionList.changeToLabelVisible(
-						winNumLabel1, winNumLabel2, winNumLabel3
-						, winNumLabel4, winNumLabel5,
+				functionList.changeToLabelVisible(winNumLabel1, winNumLabel2, winNumLabel3, winNumLabel4, winNumLabel5,
 						winNumLabel6, bonusNumLabel);
 			}
 		});
@@ -150,7 +168,7 @@ public class DialogPnl extends JDialog {
 		});
 
 		pack();
-        setSize(500, 500);
+		setSize(500, 500);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
 //	public static void main(String[] args) {
